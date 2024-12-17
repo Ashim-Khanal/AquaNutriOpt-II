ddbbfb726aa9f85084d3e68126250fe1b986b975
Ashim-Khanal <56332436+Ashim-Khanal@users.noreply.github.com>
2024-12-17 13:09:41 -0500

    WAM SO Automation
    WAM SO Automation. New functionalities to transform output from WAM and automating the input file generation for AuqaNutriOpt. New scripts and functionalities are added.
    Two Input Folders are required. The sample Input for TimePeriod = 3 can be found in the following link.
    1. BMP_database: 
    https://usfedu-my.sharepoint.com/:f:/g/personal/ashimkhanal_usf_edu/Eq9uVcLifWFAhL_DeHwGi3oBHc-uZCl8sQj-JyD_ogoeLQ?e=ZwZB7u
    2. WAM: 
    https://usfedu-my.sharepoint.com/:f:/g/personal/ashimkhanal_usf_edu/ElD5sam4hrpPky_EjOVMwM0BB_RAsqXakHiHChRNlybKCg?e=GEECeV

--- runAquaNutriOptV2.py
+++ runAquaNutriOptV2.py
@@ -1,27 +1,17 @@
 from AquaNutriOpt import *
-"""For Multi Objective Time Indexed Model (Objectives will be to minimize P, N, Budget)"""
-# An object of class EPA
-Example = EPA()
-# Read Network, BMP, and timeperiod inputs
-Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 5)
-# Set the target location for optimization
-Example.Set_TargetLocation('1')
-# Setting Budget List (Give Inputs of the budgets you wish to run experiments for)
-Example.Set_Budget_List([0, 1000000])
-# Solving the model
-Example.Solve_MOTI_Det_Model()
 
+#Example= EPA()
 
-#Example = EPA()
+""" Single Obj. Model"""
 #Example.Read_Data('Net_Data.csv','BMP_Tech.csv')
 
 #Setting the cost budget
 #Example.Set_Cost_Budget(100000)
 
 #Setting the target node
+#Example.Set_TargetLocation('1')
 
-
-#Example.Set_BoundedMeasures(['N'],[0])
+#Example.Set_BoundedMeasures(['N'], [0])
 #Example.Set_Objective('P')
 
 #Solving single objective model
@@ -30,11 +20,35 @@ Example.Solve_MOTI_Det_Model()
 
 """ Single Objective Time Indexed Model """
 
-# Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 5)
-#
-# Example.Set_TargetLocation('1')
-# Example.Set_Objective('N')
-# Example.Set_BoundedMeasures(['P'],[9999])
-# Example.Solve_SOTI_Det_Model()
+Example = EPA()
+
+Example.WAM_InputGenerator_SO('P', 3)
+
+Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 3)
+
+Example.Set_Cost_Budget(10000)
+
+Example.Set_TargetLocation('1')
+
+Example.Set_BoundedMeasures(['N'],[99999])
+
+Example.Set_Objective('P')
+
+Example.Set_TimeLimit(15)
+
+Example.Solve_SOTI_Det_Model()
 
 #Example.Set_Measure_Budget('N',99999)
+
+"""FOR Multi Objective Time Indexed Model (Objectives will be to minimize P, N, Budget)"""
+# Example = EPA()
+#
+# Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 5)
+# Example.Set_TimeLimit(15) #default value is set to 10 seconds
+#
+# # Setting Budget List (Give Inputs of the budgets you wish to run experiments for)
+# Example.Set_Budget_List([0, 100000, 500000, 1000000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000])
+# # Setting the target node
+# Example.Set_TargetLocation('1')
+# Example.Solve_MOTI_Det_Model()
+# Example.Filter_NonDominatedPoints(0.28)
