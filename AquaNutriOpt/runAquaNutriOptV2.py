de43eb84d94d5b79e63613e907e9ad72ec6cee18
James Dolinar <jamesdolinar@gmail.com>
2025-12-11 17:10:11 -0700

    Update examples in runAquaNutriOptV2.py
    - Organizes examples into functions
    - Shows how to use the new SWAT visualization
    - Updates authored by @saigontrade88
    
    Co-authored-by: @saigontrade88

--- AquaNutriOpt/runAquaNutriOptV2.py
+++ AquaNutriOpt/runAquaNutriOptV2.py
@@ -1,4 +1,8 @@
 from AquaNutriOpt import *
+
+from AquaNutriOpt.SWAT_Opt_Visualization_new import swat_opt_visualization
+from AquaNutriOpt.WAM_Opt_Visualization import wam_opt_visualization
+import os 
 #
 # Example = EPA()
 # #
@@ -21,114 +25,187 @@ from AquaNutriOpt import *
 # Example.Get_CorrectedLoad_SO()
 
 # """ Single Objective Time Indexed Model """
-# """Create an object of EPA class from the AquaNutriOpt Package"""
-# Example = EPA()
-# """Generate Optimization inputs directly from WAM outputs.
-# See section below for required WAM output files and formats.
-# 'P' is the nutrient to be optimized and 3 is the number of
-# considered time periods for nutrients loading"""
-# Example.WAM_InputGenerator_SO('P', 22)
-# """Generate Optimization inputs directly from SWAT outputs.
-# See section below for required SWAT output files and formats"""
-# # Example.SWAT_InputGenerator_SO('P', 22)
-# """Read two major input csv files. Names must be Net_Data.csv
-# and BMP_Tech.csv. InputGenerator module can create this file.
-# Else it can be self created manually without using the InputGenerator
-# Module. The format guidelines is provided later in this User Guide"""
-# Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 22)
-# """Set the available budget for BMPs/TFs for the single objective
-# optimization"""
-# Example.Set_Cost_Budget(100000000)
-# """Set the target location where objective nutrient is to be minimized.
-# If Input network file has lake as Node 1 then """
-# Example.Set_TargetLocation('46')
-# """ If we want to bound the other nutrient to a certain value say
-# Nitrogen while minimizing Phosphorus is the objective"""
-# Example.Set_BoundedMeasures(['N'],[999999999999])
-# """ In order to Set Phosphorus as the Objective"""
-# Example.Set_Objective('P')
-# """Set the TimeLimit for each optimization problem in seconds.
-# In Multiobjective optimization numerous single objective optimization iterations are run"""
-# Example.Set_TimeLimit(15)
-# # """Solve Single Objective Optimization with time periods provided"""
-# Example.Solve_SOTI_Det_Model()
-#
-# Example.Get_CorrectedLoad_SO()
-# Example.Set_Measure_Budget('N',99999)
+def run_wam_single_objective(water_shed_name, model_type, run_visualization):
 
-"""FOR Multi Objective Time Indexed Model (Objectives will be to minimize P, N, Budget)"""
+    # """Create an object of EPA class from the AquaNutriOpt Package"""
+    Example = EPA()
+    # """Generate Optimization inputs directly from WAM outputs.
+    # See section below for required WAM output files and formats.
+    # 'P' is the nutrient to be optimized and 3 is the number of
+    # considered time periods for nutrients loading"""
+    Example.WAM_InputGenerator_SO('P', "1998, 2007, 2017, 2018")
+    
+    Example.Set_TimeLimit(15)
 
-Example = EPA()
-# Example.WAM_InputGenerator_MO(3)
-Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 3)
+    Example.Set_TargetLocation('0')
 
-#default value is set to 10 seconds
-Example.Set_TimeLimit(15) # for solving each MIP optimization problem
+    Example.Set_BoundedMeasures(['N'],[99999])
 
-# # Setting the target node
-Example.Set_TargetLocation('1')
+    Example.Set_Cost_Budget(100000)
 
-# # Setting Budget List (Give Inputs of the budgets you wish to run experiments for)
-Example.Set_Budget_List([0, 100000, 500000, 1000000])
+    Example.Set_Objective('P')
 
-Example.Solve_MOTI_Det_Model()
-Example.Filter_NonDominatedPoints(0.28)
+    Example.Solve_SOTI_Det_Model()
+    
+    Example.Get_CorrectedLoad_SO()
+    Example.Set_Measure_Budget('N',99999)
 
-Example.Get_Corrected_Loads_MO(setting = 'Filtered')
-Example.GetEssential_MOoutputs()
+    if run_visualization:
+        wam_opt_visualization(
 
+            working_path=os.getcwd(),                                 # Your working directory, 
+            # should have the WAM_Opt_Visualization directory in it
 
+            aquanutriopt_output_file='Res_BMPs_SOTI_100000.txt',    
+            # The name of your AquaNutriOpt output file.
+            # Will be something like 'Res_BMPs_SOTI_100000.0.txt'. Should be located
+            # at <working_path>/WAM_Opt_Visualization/Inputs/<aquanutriopt_output_file>
+
+            lookup_table_file='LOOKUP_Table.csv', # The name of your lookup table file, 
+            # located at <working_path>/WAM_Opt_Visualization/Inputs/<lookup_table_file>
+
+            land_use_subbasin_file='Land_Subbasin_Intersection.shp',  
+            # The name of your land use 
+            # subbasin intersection shapefile, 
+
+                                                                    # located at <working_path>/WAM_Opt_Visualization/Inputs/From_WAM_Model_Generation_Script/<land_use_subbasin_file>
+
+            watershed_boundary_file='Watershed.shp',          
+            # The name of your watershed boundary
+            # shapefile,
+
+                                                                    # located at <working_path>/WAM_Opt_Visualization/Inputs/Watershed/<watershed_boundary_file>
+
+            out_file_basename='WAM_SO_visualization_output'           # The name of your ouput files 
+            #(without the extension).
+
+        )
 
 # """ SWAT Single Objective Time Indexed Model """
 # """Create an object of EPA class from the AquaNutriOpt Package"""
-# Example = EPA()
-# """Generate Optimization inputs directly from WAM outputs.
-# See section below for required WAM output files and formats.
-# 'P' is the nutrient to be optimized and 3 is the number of
-# considered time periods for nutrients loading"""
-# Example.SWAT_InputGenerator_SO('P', 22)
-# """Generate Optimization inputs directly from SWAT outputs.
-# See section below for required SWAT output files and formats"""
-# #Example.SWAT_InputGenerator_SO('N', 22)
-# """Read two major input csv files. Names must be Net_Data.csv
-# and BMP_Tech.csv. InputGenerator module can create this file.
-# Else it can be self created manually without using the InputGenerator
-# Module. The format guidelines is provided later in this User Guide"""
-# Example.Read_Data('Net_Data.csv','BMP_Tech.csv',22)
-# """Set the available budget for BMPs/TFs for the single objective
-# optimization"""
-# Example.Set_Cost_Budget(10000000)
-# """Set the target location where objective nutrient is to be minimized.
-# If Input network file has lake as Node 1 then """
-# Example.Set_TargetLocation('46')
-# """ If we want to bound the other nutrient to a certain value say
-# Nitrogen while minimizing Phosphorus is the objective"""
-# Example.Set_BoundedMeasures(['N'],[999])
-# """ In order to Set Phosphorus as the Objective"""
-# Example.Set_Objective('P')
-# """Set the TimeLimit for each optimization problem in seconds.
-# In Multi-objective optimization numerous single objective optimization
-#  are run"""
-# Example.Set_TimeLimit(15)
-# """Solve Single Objective Optimization with time periods provided"""
-# Example.Solve_SOTI_Det_Model()
-#
-# #Example.Set_Measure_Budget('N',99999)
+def run_swat_single_objective(water_shed_name, model_type, run_visualization):
+
+    if not run_visualization:
+        Example = EPA()
+
+        Example.SWAT_InputGenerator_SO('P', '2002, 2003')
+        # Example.SWAT_InputGenerator_SO('N', '2002, 2003')
+
+        # """Set the available budget for BMPs/TFs for the single objective
+        # optimization"""
+        # """Set the target location where objective nutrient is to be minimized.
+        # If Input network file has lake as Node 1 then """
+        Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 2)
+        # """ If we want to bound the other nutrient to a certain value say
+        # Nitrogen while minimizing Phosphorus is the objective"""
+        Example.Set_TargetLocation('99999')
+        
+        Example.Set_BoundedMeasures(['N'],[99999])
+
+        Example.Set_Cost_Budget(100000)
+
+        # """ In order to Set Phosphorus as the Objective"""
+        Example.Set_Objective('P')
+        # """Set the TimeLimit for each optimization problem in seconds.
+        # In Multi-objective optimization numerous single objective optimization
+        #  are run"""
+        Example.Set_TimeLimit(15)
+        # # """Solve Single Objective Optimization with time periods provided"""
 
 
-"""FOR SWAT Multi Objective Time Indexed Model (Objectives will be to minimize P, N, Budget)"""
+        Example.Solve_SOTI_Det_Model()
 
-# Example = EPA()
-# #Example.WAM_InputGenerator_MO(22)
-# Example.SWAT_InputGenerator_MO(22)
-# Example.Read_Data('Net_Data.csv','BMP_Tech.csv', 22)
-#
-# #default value is set to 10 seconds
-# Example.Set_TimeLimit(15) # for solving each MIP optimization problem
-#
-# # # Setting Budget List (Give Inputs of the budgets you wish to run experiments for)
-# Example.Set_Budget_List([0, 100000, 500000, 1000000])
-# # # Setting the target node
-# Example.Set_TargetLocation('46')
-# Example.Solve_MOTI_Det_Model()
-# Example.Filter_NonDominatedPoints(0.28)
+    # Example.Get_CorrectedLoad_SO()
+    # Example.Set_Measure_Budget('N',99999)
+
+    # Add this call here, you may have to update parameters as needed
+    else:
+        swat_opt_visualization(
+
+            working_path=os.getcwd(),                                 # Your working directory, 
+            # should have the WAM_Opt_Visualization directory in it
+
+            aquanutriopt_output_file='Res_BMPs_SOTI_100000.txt',    # The name of your AquaNutriOpt output file.
+            # aquanutriopt_output_file='BMPs_1500000bound_N_4978.348.txt',
+            # Will be something like 'Res_BMPs_SOTI_100000.0.txt'. Should be located
+                        # at <working_path>/WAM_Opt_Visualization/Inputs/<aquanutriopt_output_file>
+
+                                                                    # at <working_path>/WAM_Opt_Visualization/Inputs/<aquanutriopt_output_file>
+            WAM_lookup_table_file='LOOKUP_Table.csv',
+            SWAT_lookup_table_file='SWATWAMLUIDs_LOOKUP.csv',                     # The name of your lookup table file, 
+            # located at <working_path>/WAM_Opt_Visualization/Inputs/<lookup_table_file>
+
+            land_use_subbasin_file='Land_Subbasin_Intersection.shp',  # The name of your land use 
+            # subbasin intersection shapefile, 
+                                                              # located at <working_path>/WAM_Opt_Visualization/Inputs/From_WAM_Model_Generation_Script/<land_use_subbasin_file>
+            watershed_boundary_file='Watershed.shp',                  # The name of your watershed boundary
+            # shapefile,
+
+                                                                    # located at <working_path>/WAM_Opt_Visualization/Inputs/Watershed/<watershed_boundary_file>
+            out_file_basename='SWAT_SO_visualization_output'               # The name of your ouput files 
+        #(without the extension).
+
+        )       
+
+        
+# create a main function to run the script
+if __name__ == "__main__":
+    # the function 
+    working_path=os.getcwd()
+    # Users from the terminal call python runAquaNutriOptV2.py --model SWAT --visualization the type of models and whether they want to run visualization
+    water_shed_name = 'Mar Menor Catchment'
+    model_type = 'SWAT' # 'EPA_SO', 'EPA_MO',
+    run_visualization = True
+    if model_type == 'SWAT':
+        if run_visualization:
+            run_swat_single_objective(water_shed_name, 
+                                      model_type, 
+                                      run_visualization=True)
+        else:
+            run_swat_single_objective(water_shed_name,
+                                      model_type, 
+                                      run_visualization=False)
+    
+    elif model_type == 'WAM':
+        if run_visualization:
+            run_wam_single_objective(water_shed_name, 
+                                     model_type, 
+                                     run_visualization=True)
+        else:
+            run_wam_single_objective(water_shed_name, 
+                                     model_type, 
+                                     run_visualization=False)
+    elif model_type == 'custom':
+        if run_visualization:
+            # call your custom model function with visualization
+            wam_opt_visualization(
+
+                working_path=os.getcwd(),                                 # Your working directory, 
+                # should have the WAM_Opt_Visualization directory in it
+
+                aquanutriopt_output_file='Res_BMPs_SOTI_100000.txt',    
+                # The name of your AquaNutriOpt output file.
+                # Will be something like 'Res_BMPs_SOTI_100000.0.txt'. Should be located
+                # at <working_path>/WAM_Opt_Visualization/Inputs/<aquanutriopt_output_file>
+
+                lookup_table_file='Custom_Model_LOOKUP_Table.csv', # The name of your lookup table file, 
+                # located at <working_path>/WAM_Opt_Visualization/Inputs/<lookup_table_file>
+
+                land_use_subbasin_file='Land_Subbasin_Intersection.shp',  
+                # The name of your land use 
+                # subbasin intersection shapefile, 
+
+                # located at <working_path>/WAM_Opt_Visualization/Inputs/From_WAM_Model_Generation_Script/<land_use_subbasin_file>
+                watershed_boundary_file='Watershed.shp',          
+                # The name of your watershed boundary
+                # shapefile,
+                 # located at <working_path>/WAM_Opt_Visualization/Inputs/Watershed/<watershed_boundary_file>
+                out_file_basename='Custom_model_SO_visualization_output'           # The name of your ouput files 
+            #(without the extension).
+
+        )
+
+    else:
+        print("Invalid model type. Please choose from 'WAM', 'SWAT', or 'custom'.")
+        exit(1)
+
